03de7664124d522c38756112669b98efae490064
Frantisek Pavlik <pavf05@vse.cz>
2026-03-10 15:22:59 +0100

    Remove legacy subcategory and photo items from content structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -6,10 +6,6 @@ export const structure: StructureResolver = (S) =>
     .title('Content')
     .items([
       S.documentTypeListItem('category').title('Categories'),
-      S.divider(),
-      S.documentTypeListItem('subcategory').title('Legacy subcategories'),
-      S.documentTypeListItem('photo').title('Legacy photos'),
-      S.divider(),
       ...S.documentTypeListItems().filter(
         (item) => item.getId() && !['category', 'subcategory', 'photo'].includes(item.getId() as string)
       ),
